be42e43d8e6199060012f1a9411dcf6966282ecc
wcaj <gamezerca@gmail.com>
2024-02-04 17:19:31 -0500

    Initial commit

---
+++ src/main/java/com/jucaye/jucaye/JucayeApplication.java
@@ -0,0 +1,13 @@
+package com.jucaye.jucaye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JucayeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JucayeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jucaye/jucaye/JucayeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jucaye.jucaye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JucayeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
